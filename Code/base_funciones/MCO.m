dbeba4cfe93a15fc346ceb652c663bfeedad8776
Hriday Karnani <hkarnani@fen.uchile.cl>
2024-03-26 13:34:31 -0300

    Add files via upload

--- Code/base_funciones/MCO.m
+++ Code/base_funciones/MCO.m
@@ -3,7 +3,7 @@ function [beta,ee,t_stat] = MCO(X,Y)
     error = Y - X*beta;  % estos son los residuos
     T = size(X,1); % nro observaciones
     N = size(X,2); % nro regresores
-    ee = 1./(T-N)*(diag(error'*error)\(X'*X)); % errores estandar
-    ee = diag(ee); % nos interesa solo la diagonal de la matriz var-cov (por ahora)
+    ee = (1./(T-N))*((error'*error)\(X'*X)); % errores estandar
+    ee = sqrt(diag(ee)); % nos interesa solo la diagonal de la matriz var-cov (por ahora)
     t_stat = beta./ee;
 end
